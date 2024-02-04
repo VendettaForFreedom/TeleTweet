2b6b3417209e469b513ab7b359ac950cbace5b8d
frauen-leben-freiheit <157930273+frauen-leben-freiheit@users.noreply.github.com>
2024-02-04 23:47:43 +0100

    adding new utils to .ci_utils

---
+++ .ci_utils/create_env_file.sh
@@ -0,0 +1,15 @@
+if [ -e env/teletweet.env ]; then
+    rm env/teletweet.env
+fi
+echo echo TOKEN=${{ secrets.TOKEN}} >> env/teletweet.env
+echo CONSUMER_KEY=${{ secrets.CONSUMER_KEY}} >> env/teletweet.env
+echo CONSUMER_SECRET=${{ secrets.CONSUMER_SECRET}} >> env/teletweet.env
+echo #CALLBACK_URL=  >> env/teletweet.env
+echo APP_ID=${{ secrets.APP_ID}} >> env/teletweet.env
+echo APP_HASH=${{ secrets.APP_HASH}} >> env/teletweet.env
+echo ACCESS_KEY=${{ secrets.ACCESS_KEY}} >> env/teletweet.env
+echo ACCESS_SECRET=${{ secrets.ACCESS_SECRET}} >> env/teletweet.env
+echo CONFIG_CHANNEL_ID=${{ secrets.CONFIG_CHANNEL_ID}} >> env/teletweet.env
+echo CHANNEL_ID=${{ secrets.CHANNEL_ID}} >> env/teletweet.env
+echo ALLOW_USER=${{ secrets.ALLOW_USER}} >> env/teletweet.env
+
