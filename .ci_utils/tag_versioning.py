986d6fb99781aca2cc3cc2da49c10812871ff13b
frauen-leben-freiheit <157930273+frauen-leben-freiheit@users.noreply.github.com>
2024-02-26 11:26:34 +0100

    updating tag_versioning.py

--- .ci_utils/tag_versioning.py
+++ .ci_utils/tag_versioning.py
@@ -6,6 +6,15 @@ __author__ = "Frauen-Leben-Freiheit"
 __version__ = "0.0.1"
 
 import os
+import git
+
+
+cwd = os.getcwd()
+parent_dir = os.path.dirname(cwd)
+print(f"Current working directory: {cwd}")
+print(f"Parent directory: {parent_dir}")
+the_repo = git.Repo(parent_dir, search_parent_directories=True).git
+the_branch = os.getenv("CI_COMMIT_BRANCH")
 
 # if the bump_type is bump_patch, it will bump the patch version
 # if the bump_type is bump_minor, it will bump the minor version
@@ -19,11 +28,12 @@ class TagBump:
         """
         if latest_tag[0] != "V":
             raise ValueError("The tag should start with V")
-        if latest_tag.split('.').count != 3:
+        if len(latest_tag.split('.')) != 3:
             raise ValueError("The tag should have 3 parts")
         self.latest_tag = latest_tag
-        self.version = latest_tag[1:].split('.')
+        self.version = [int(val) for val in latest_tag[1:].split('.')]
         self.bump_type = bump_type
+        self.updated_tag = None
     
     def _bump_minor(self):
         """Bump the minor version"""
@@ -50,11 +60,25 @@ class TagBump:
             self._bump_major()
         else:
             raise ValueError("The bump_type should be bump_patch, bump_minor or bump_major")
-        return f"V{'.'.join(self.version)}"
+        self.updated_tag = f"V{'.'.join([str(val) for val in self.version])}"
+        return self.updated_tag
 
+    def apply_tag(self):
+        """Apply the tag to the repo"""
+        the_repo.tag(self.updated_tag,
+            ref=the_branch, message=f"Version {self.updated_tag} is created")
+        the_repo.push("--tags")
+        # the_repo.git.push(the_branch)
 
 if __name__ == "__main__":
     latest_tag = os.environ.get("LATEST_TAG")
     bump_type = os.environ.get("BUMP_TYPE")
+    print(f"LATEST_TAG: {latest_tag}")
+    print(f"BUMP_TYPE: {bump_type}")    
     tag_bump = TagBump(latest_tag, bump_type)
-    os.environ["UPDATED_TAG"] = tag_bump.bump_version()
+    UPATED_TAG = tag_bump.bump_version()
+    print(f"UPATED_TAG: {UPATED_TAG}")
+    os.environ["UPDATED_TAG"] = UPATED_TAG
+    print(f"{os.getenv('UPDATED_TAG')}")
+    tag_bump.apply_tag()
+
