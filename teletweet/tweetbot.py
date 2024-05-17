d872b5c9bbb0df73bbfc6c690e595eecbd723906
VendettaForFreedom <>
2024-05-17 19:36:59 +0300

    feature-customize

--- teletweet/tweetbot.py
+++ teletweet/tweetbot.py
@@ -320,8 +320,6 @@ def send_ad_message(message):
 
 def send_config_message(part):
     try:
-        logging.info("fetched_messages before")
-
         # read message_id_pairs from file randomly
         # Open and read the text file
         with open('message_id_pairs.txt', 'r') as file:
@@ -331,6 +329,7 @@ def send_config_message(part):
         selected_pair = random.choice(message_id_pairs)
         logging.info("selected_pair: %s", selected_pair)
 
+        logging.info("fetched_messages before")
         content, picture, chat_id, img_data = "", "", "", None
         if selected_pair is not None:
             fetched_messages = bot.get_messages(SOURCE_REPOSITORY_CHANNEL_ID, selected_pair)
@@ -342,6 +341,7 @@ def send_config_message(part):
                     picture = msg.photo.file_id
                     img_data = msg.download(in_memory=True)
                     setattr(img_data, "mode", "rb")
+        logging.info("fetched_messages after")
         
         message_body = ""
         if chat_id is not None:
